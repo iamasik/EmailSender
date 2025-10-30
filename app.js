e40e076dc71fcfaac758d7d411ce6241c44e1a05
Md. Delwar Hossen <delwarhossenasik@gmail.com>
2025-10-30 15:18:12 +0600

    fix: routing for netlify

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/.netlify/functions/api', router);
+app.use(router);
 
 // This will run the server locally when you run `npm run dev`
 if (require.main === module) {
