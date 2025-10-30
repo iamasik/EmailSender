ec9b366a1f5f0af974a1f4fcd9449df00e501ca3
Md. Delwar Hossen <delwarhossenasik@gmail.com>
2025-10-30 15:11:20 +0600

    refactor: adapt app for serverless deployment

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
-const emailController = require('./controllers/emailController');
+const emailController = require('../controllers/emailController');
 
 const app = express();
+const router = express.Router();
+
+router.get('/', emailController.renderIndex);
+router.post('/send', emailController.sendEmail);
+router.post('/api/send', emailController.apiSendEmail);
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-const upload = multer({ dest: 'uploads/' });
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', emailController.renderIndex);
-app.post('/send', emailController.sendEmail);
-app.post('/api/send', emailController.apiSendEmail);
+app.use('/.netlify/functions/api', router);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+module.exports = app;
